d35e9403ccba14f5de8ce1a08539f65cb14e9dcf
deepali04 <nagwade.deepali@gmail.com>
2024-03-18 20:11:08 -0400

    Updates

---
+++ precache-manifest.908f26636d65e029e195a4b375723c17.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16ef42ad23a5b46ed2fdbb95e33ad2c",
+    "url": "/Trivia-Pulse/index.html"
+  },
+  {
+    "revision": "8f08aaabbcab11f4610e",
+    "url": "/Trivia-Pulse/static/css/2.6f2db2d8.chunk.css"
+  },
+  {
+    "revision": "1260d6acf30ca81fd3af",
+    "url": "/Trivia-Pulse/static/css/main.b96d5e1d.chunk.css"
+  },
+  {
+    "revision": "8f08aaabbcab11f4610e",
+    "url": "/Trivia-Pulse/static/js/2.4557bd43.chunk.js"
+  },
+  {
+    "revision": "d23d5b3a569f6a44e4cec3f2a0280caa",
+    "url": "/Trivia-Pulse/static/js/2.4557bd43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ff2c6547e14d80e6ad",
+    "url": "/Trivia-Pulse/static/js/3.c5eb4ea7.chunk.js"
+  },
+  {
+    "revision": "1260d6acf30ca81fd3af",
+    "url": "/Trivia-Pulse/static/js/main.1ae429a6.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Trivia-Pulse/static/js/main.1ae429a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "809149eb7d52facdeecb",
+    "url": "/Trivia-Pulse/static/js/runtime-main.a625b240.js"
+  }
+]);
